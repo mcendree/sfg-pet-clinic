700fdcd879b3f8cf5f13a552173adf9c81aa8605
Trebuche <mcendree_adam@yahoo.com>
2021-03-24 12:00:59 -0500

    catch up update

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,18 +1,31 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+/**
+ * Created by jt on 7/18/18.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    public Long getId() {
-
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public boolean isNew() {
+        return this.id == null;
+    }
 }
--- pet-clinic-data/src/main/java/services/PetService.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-import guru.springframework.sfgpetclinic.model.Pet;
-
-public interface PetService extends CrudService<Pet, Long>{
-
-}
